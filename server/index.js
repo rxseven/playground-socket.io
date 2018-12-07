77cacceb29fa5e184b938f321287f5db268f3325
ME (Mac mini) <rxseven.com@gmail.com>
2018-12-07 13:37:59 +0700

    Configure HTTP server & Express app

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ const bodyParser = require('body-parser');
 const config = require('config');
 const cors = require('cors');
 const express = require('express');
+const http = require('http');
 const morgan = require('morgan');
 const path = require('path');
 
 // Public path
 const publicPath = path.join(__dirname, '../public');
 
-// Create Express server
+// Initialize app to be a function handler
 const app = express();
+
+// Create an HTTP server object
+const server = http.createServer(app);
 
 // Logger
 if (config.util.getEnv('NODE_ENV') !== 'test') {
@@ -27,7 +31,7 @@ app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
 // Bind and listen for connections on the specified host and port
-app.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log('Server is listening on port 5000');
 });
 
