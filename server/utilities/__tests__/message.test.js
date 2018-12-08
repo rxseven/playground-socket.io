519b829c9481af257b12fbc2fba9b4fe422bf91e
ME (Mac mini) <rxseven.com@gmail.com>
2018-12-08 14:09:16 +0700

    Add test suites for generateLocation function

--- server/utilities/__tests__/message.test.js
+++ server/utilities/__tests__/message.test.js
@@ -1,7 +1,7 @@
 // Module dependencies
 const expect = require('expect');
 
-const { generageMessage } = require('../message');
+const { generageMessage, generateLocation } = require('../message');
 
 // Test suite
 describe('generateMessage', () => {
@@ -16,6 +16,25 @@ describe('generateMessage', () => {
     expect(message).toMatchObject({
       from,
       text
+    });
+  });
+});
+
+// Test suite
+describe('generateMessage', () => {
+  it('should generate correct location object', () => {
+    // Variables
+    const from = 'Admin';
+    const latitude = 13.4513253;
+    const longitude = 144.508632;
+    const url = 'https://www.google.com/maps?q=13.4513253,144.508632';
+    const message = generateLocation(from, latitude, longitude);
+
+    // Assertions
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({
+      from,
+      url
     });
   });
 });
