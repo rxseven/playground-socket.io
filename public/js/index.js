35d4517803243cea771fbda7d8f2a29fd6c8da5f
ME (Mac mini) <rxseven.com@gmail.com>
2018-12-08 12:28:37 +0700

    Add event listener for receiving new message from server

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,18 @@ socket.on('disconnect', function() {
   console.log('Socket.io - Disconnected from the server');
 });
 
+// Listen for new message
+socket.on('newMessage', function(message) {
+  // Create a list element
+  var li = $('<li></li>');
+
+  // Set the content of the list element to the specified message.
+  li.text(message.from + ': ' + message.text);
+
+  // Insert content to the end of the list element.
+  $('#message-list').append(li);
+});
+
 // Form
 $('#message-form').on('submit', function(event) {
   // Prevent the form from being submitted
