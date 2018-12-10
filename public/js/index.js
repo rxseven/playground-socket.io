5d7143e6c4678389e9f47c66860f20a57765de5c
ME (Mac mini) <rxseven.com@gmail.com>
2018-12-10 11:24:25 +0700

    Add function for scrolling message list to the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,27 @@
 // Initialize Socket.io connection
 var socket = io('http://localhost:5000');
 
+// Scroll to bottom
+function scrollBottom() {
+  // Selectors
+  var $messages = $('#message-list');
+  var $newMessage = $messages.children('li:last-child');
+
+  // DOM properties
+  var clientHeight = $messages.prop('clientHeight');
+  var scrollTop = $messages.prop('scrollTop');
+  var scrollHeight = $messages.prop('scrollHeight');
+
+  // Heights
+  var newMessageHeight = $newMessage.innerHeight();
+  var lastMessageHeight = $newMessage.prev().innerHeight();
+
+  // Scrolling condition
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    $messages.scrollTop(scrollHeight);
+  }
+}
+
 // Connected to the server
 socket.on('connect', function() {
   console.log('Socket.io - Connected to the server');
