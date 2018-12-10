eff3fecd4e052c79d7d94104d81054a679794038
ME (MBP) <rxseven.com@gmail.com>
2018-12-10 15:28:44 +0700

    Add Users class's method for adding new user

--- server/utilities/users.js
+++ server/utilities/users.js
@@ -4,6 +4,18 @@ class Users {
   constructor() {
     this.users = [];
   }
+
+  // Add user
+  addUser(id, name, room) {
+    // Create a new user
+    const user = { id, name, room };
+
+    // Add new user to the list
+    this.users.push(user);
+
+    // Return new user
+    return user;
+  }
 }
 
 // Module exports
